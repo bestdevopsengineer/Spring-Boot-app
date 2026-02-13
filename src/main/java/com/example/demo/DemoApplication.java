9aac8fabea09ccbe7f7d468f71a93ae826c6d719
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-02-13 11:43:10 -0600

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo; // update if your package is different
+package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
-public class HomeController {
+@SpringBootApplication
+public class DemoApplication {
 
-    @GetMapping("/")
-    public String home() {
-        return "App is running!";
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
 }
